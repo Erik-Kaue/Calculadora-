5f7ee27818bc477cf9a94068fde1eaf97b3e7ede
Erik-Kaue <dexplay122@hotmail.com>
2019-06-03 22:18:06 -0300

    inicio do projeto

---
+++ Calculadora/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSoma_Click(object sender, EventArgs e)
+        {
+            double vl1, vl2, res;
+
+            vl1 = double.Parse(txtValor1.Text);
+            vl2 = double.Parse(txtValor2.Text);
+
+            res = vl1 + vl2;
+
+            txtResultado.Text = res.ToString();
+        }
+
+        private void btnSub_Click(object sender, EventArgs e)
+        {
+            double vl1, vl2, res;
+
+            vl1 = double.Parse(txtValor1.Text);
+            vl2 = double.Parse(txtValor2.Text);
+
+            res = vl1 - vl2;
+
+            txtResultado.Text = res.ToString();
+        }
+
+        private void btnDiv_Click(object sender, EventArgs e)
+        {
+            double vl1, vl2, res;
+
+            vl1 = double.Parse(txtValor1.Text);
+            vl2 = double.Parse(txtValor2.Text);
+
+            res = vl1 / vl2;
+
+            txtResultado.Text = res.ToString();
+        }
+
+        private void btnMult_Click(object sender, EventArgs e)
+        {
+            double vl1, vl2, res;
+
+            vl1 = double.Parse(txtValor1.Text);
+            vl2 = double.Parse(txtValor2.Text);
+
+            res = vl1 * vl2;
+
+            txtResultado.Text = res.ToString();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
